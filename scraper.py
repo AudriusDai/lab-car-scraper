76c6b9873b09861edc0fa3b6771224643d4e4c1a
Audrius Dailyde <audrius.dailyde@revelsystems.com>
2020-07-11 22:41:08 +0300

    add more data to text file

--- scraper.py
+++ scraper.py
@@ -1,23 +1,47 @@
 from lxml import html
 from datetime import datetime
+from timeit import default_timer
 
 import requests
+import time
 
-page_number = 1
-total_pages = 5
-all_sold_cars_hrefs = []
 
-while page_number != total_pages:
-    page = requests.get(f'https://autoplius.lt/skelbimai/naudoti-automobiliai?page_nr={page_number}')
-    tree = html.fromstring(page.content)
-    sold_cars = tree.xpath('//a[@class="announcement-item is-sold is-inactive"]')
-    for car in sold_cars:
-        href = car.attrib.get('href')
-        print(f'Page {page_number}. Sold car href:', car.attrib.get('href'))
-        all_sold_cars_hrefs.append(href)
-    page_number += 1
+def main(page_start: int, page_end: int):
+    start_time = default_timer()
+    all_sold_cars_hrefs = []
+    page_number = page_start
 
-f = open(f"cars/sold-cars-{datetime.utcnow()}.txt", "a")
-for car_href in all_sold_cars_hrefs:
-    f.write(f'{car_href}\n')
-f.close()
+    while page_number != page_end + 1:
+        print(f'Scraping page {page_number}')
+        page = requests.get(f'https://autoplius.lt/skelbimai/naudoti-automobiliai?page_nr={page_number}')
+        tree = html.fromstring(page.content)
+        sold_cars_hrefs = tree.xpath('//a[@class="announcement-item is-sold is-inactive"]')
+        sold_cars_photos = tree.xpath(
+            '//a[@class="announcement-item is-sold is-inactive"]/div[@class="announcement-media"]/div[@class="announcement-photo"]/img[1]')
+        sold_cars_prices = tree.xpath(
+            '//a[@class="announcement-item is-sold is-inactive"]/div[@class="announcement-body"]/div[@class="announcement-pricing-info"]/strong')
+        sold_cars_titles = tree.xpath(
+            '//a[@class="announcement-item is-sold is-inactive"]/div[@class="announcement-body"]/div[@class="announcement-title"]')
+        for i in range(len(sold_cars_hrefs)):
+            href = sold_cars_hrefs[i].attrib.get('href')
+            image_src = sold_cars_photos[i].attrib.get('data-src')
+            price = sold_cars_prices[i].text.strip()
+            title = sold_cars_titles[i].text.strip()
+            print(f'Page {page_number}. Sold car href:{href}. Image url: {image_src}. Price: {price}. Title: {title}')
+            all_sold_cars_hrefs.append(f'{href} {title} {price} {image_src}')
+        page_number += 1
+        if page_number % 100 == 0:
+            print('Sleeping for 2secs..')
+            time.sleep(2)
+            print('Continuing on!')
+
+    all_sold_cars_hrefs.sort()
+    f = open(f"cars/sold-cars-{datetime.utcnow()}.txt", "a")
+    for car_href in all_sold_cars_hrefs:
+        f.write(f'{car_href}\n')
+    f.close()
+    elapsed = default_timer() - start_time
+    print('Elapsed time in seconds: ', elapsed)
+
+
+main(page_start=1, page_end=1975)

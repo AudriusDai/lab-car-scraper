8022911817fa9732f87a65579b11f8d9f3ade3eb
Audrius Dailyde <audrius.dailyde@revelsystems.com>
2020-07-11 11:05:35 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+## startup
+```
+> . venv/bin/activate
+> pip install requests
+> pip freeze > requirements.txt
+> deactivate
+```
+
+## maintain
+```
+> . venv/bin/activate
+> pip install -r requirements.txt
+> deactivate
+```
---
+++ scraper.py
@@ -0,0 +1,25 @@
+from lxml import html
+from datetime import datetime
+
+import requests
+
+page_number = 248
+total_pages = 1982
+all_sold_cars_hrefs = []
+
+while page_number != total_pages:
+    page = requests.get(f'https://autoplius.lt/skelbimai/naudoti-automobiliai?page_nr={page_number}')
+    tree = html.fromstring(page.content)
+    sold_cars = tree.xpath('//a[@class="announcement-item is-sold is-inactive"]')
+    for car in sold_cars:
+        href = car.attrib.get('href')
+        print(f'Page {page_number}. Sold car href:', car.attrib.get('href'))
+        all_sold_cars_hrefs.append(href)
+    page_number += 1
+
+
+
+f = open(f"cars/sold-cars-{datetime.utcnow()}.txt", "a")
+for car_href in all_sold_cars_hrefs:
+    f.write(f'{car_href}\n')
+f.close()
